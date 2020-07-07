6d229345c931b6f8818e1c852c1557f00106b3c9
Dec <31135263+iCielin@users.noreply.github.com>
2020-07-07 22:54:13 +0100

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -7,14 +7,5 @@ with open("sole_nyu/sole_nyu.json", "r", encoding = "utf-8") as file:
     read = json.load(file)
 data = read["GraphImages"][0]["comments"]["data"]
 array = []
-total = 0
-yes = 0
-no = 0
 for comment in data:
-    total += 1
-    if "C" in comment["text"]:
-        yes += 1
-    else:
-        no += 1
-total_percentage = str((yes / (yes + no)) * 100)[:5]
-print(f"{total_percentage}% of people voted C.")
+    array.append(comment["text"])
